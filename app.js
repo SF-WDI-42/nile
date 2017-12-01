4c19fd9f08e62c5c6d7b439b950fa127fe716c6b
mnfmnfm <mnfmnfm@gmail.com>
2017-12-01 12:21:08 -0800

    switch to template literals for products

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log("app.js linked")
+
+$(document).ready(function() {
+  let products = [
+    {
+      name: "Computers",
+      price: 1000,
+      description: "Macs are the coolest and best computers currently. Also costly."
+    },
+    {
+      name: "Cars",
+      price: 40000,
+      description: "Mercedes are speedy and fashionable."
+    },
+    {
+      name: "Plants",
+      price: 15,
+      description: "Succulents are really easy to take care of and pretty cheap."
+    }
+  ];
+  for(let i = 0; i<products.length; i++) {
+    $(".products-container").append(`<div><h4 class="product-title">${products[i].name}</h4><p>${products[i].description}</p>$${products[i].price}</div>`)
+  }
+})
